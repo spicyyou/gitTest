1fbda7cc89ed60dde533cc19dd2409988c414ba3
spicyyou <hyj5419@naver.com>
2023-01-18 15:28:27 +0900

    fix first line in second branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("first line");
+        System.out.printf("fix first line");
         System.out.println("new Commit");
         System.out.printf("second line");
     }
