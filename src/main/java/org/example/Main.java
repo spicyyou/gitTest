051e146ac7e424ea6658ecad73947f6dd8e9de5f
spicyyou <hyj5419@naver.com>
2023-01-18 15:26:55 +0900

    new command

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("first line");
         System.out.println("Init Commit");
+        System.out.printf("second line");
     }
 }
