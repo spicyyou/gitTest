50a917d04e801cbd11cab7a827a963654ea0b661
spicyyou <hyj5419@naver.com>
2023-01-18 14:58:21 +0900

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("commit first");
+        System.out.println("new Commit");
     }
 }
